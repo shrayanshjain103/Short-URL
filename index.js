aeed0b6a4a1ef7ffa9776b7cfe369841f676522d
Shrayansh Jain <shrayanshjain103@gmail.com>
2024-01-21 15:44:21 +0530

    Add the front end and ejs template engine

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
+
+const path=require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
+
+const staticRoute=require("./routes/staticRouter")
 const app = express();
 const PORT = 8001;
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("mongodb is connected")
 );
+app.use("/",staticRoute);
+
+
 
 app.use("/url", urlRoute);
-app.get("/:shortid", async (req, res) => {
+app.get("/url/:shortid", async (req, res) => {
   const shortId = req.params.shortid;
   const entry = await URL.findOneAndUpdate(
     {
